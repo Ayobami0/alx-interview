ca0176ed3a077719a464d8841cdf027f0a251c47
Ayobami <oludemiayobami@gmail.com>
2024-05-02 21:13:16 +0100

    feat: checker fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,7 +3,6 @@
     Reads stdin line by line.
 """
 
-import signal
 import sys
 import re
 
@@ -29,36 +28,33 @@ def _print_stats():
         "File size: {}".format(_total_size),
         "\n".join(["{}: {}".format(k, v) for k, v in _store.items()]),
         sep="\n",
-        end="\n",
+        flush=True,
     )
 
 
-def _handler(signum, frame):
+try:
+    for line in sys.stdin:
+        if _counter == 10:
+            _print_stats()
+            _counter = 0
+            continue
+
+        matched = _reg.search(line.strip())
+
+        if not matched:
+            continue
+
+        try:
+            file_size = int(matched.group(4))
+            st_code = int(matched.group(3))
+        except Exception:
+            continue
+
+        if _store.get(st_code, None) is None:
+            continue
+
+        _store[st_code] += 1
+        _total_size += file_size
+        _counter += 1
+finally:
     _print_stats()
-
-
-signal.signal(signal.SIGINT, _handler)
-
-for line in sys.stdin:
-    if _counter == 10:
-        _print_stats()
-        _counter = 0
-        continue
-
-    matched = _reg.search(line.strip())
-
-    if not matched:
-        continue
-
-    try:
-        file_size = int(matched.group(4))
-        st_code = int(matched.group(3))
-    except Exception:
-        continue
-
-    if _store.get(st_code, None) is None:
-        continue
-
-    _store[st_code] += 1
-    _total_size += file_size
-    _counter += 1
