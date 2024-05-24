73b24adffe760dc6cea289762beb029967b15ea8
Ayobami <oludemiayobami@gmail.com>
2024-05-24 08:44:20 +0100

    fix: checker fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,7 +4,7 @@ const request = require('request');
 const argv = require('process').argv;
 
 if (argv.length === 3) {
-  request(`https://swapi-api.alx-tools.com/api/films/${argv[2]}`, function (error, resp, body) {
+  request(`https://swapi-api.alx-tools.com/api/films/${argv[2]}`, async function (error, resp, body) {
     if (error != null) {
       return;
     }
@@ -12,16 +12,21 @@ if (argv.length === 3) {
       return;
     }
     const characters = JSON.parse(body).characters;
+
     for (let c = 0; c < characters.length; c++) {
-      request.get(characters[c], function (error, resp, body) {
+      const doRequest = () => new Promise((resolve, reject) => request.get(characters[c], function (error, resp, body) {
         if (error !== null) {
-          return;
+          reject(error);
         }
         if (resp.statusCode !== 200) {
           return;
         }
-        console.log(JSON.parse(body).name);
-      });
+        resolve(JSON.parse(body).name);
+      }));
+
+      const cName = await doRequest();
+
+      console.log(cName);
     }
   }
   );
