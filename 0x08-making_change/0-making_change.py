f24894893275d4453f431b0f31ca9f2a014e49b0
Ayobami <oludemiayobami@gmail.com>
2024-06-07 03:51:30 +0100

    docs: add documentations

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+"""0. Change comes from within"""
 
 
 def makeChange(coins, total):
-    """
-    Uses greedy algorithm to get the optimum number of coins to make
-    a change
-    """
+    """Given a pile of coins of different values, determine
+    the fewest number of coins needed to meet a given amount total"""
     if total <= 0:
         return 0
 
